afd4084d7e908cf60aae7f35e096c8f5cf386884
Nikhil <tymnec@gmail.com>
2023-12-19 16:07:33 +0530

    button added

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
 };
 
 export default Home;
