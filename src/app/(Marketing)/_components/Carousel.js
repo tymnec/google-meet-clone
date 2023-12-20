7084f22aa58a6d8a3a026d4770f421464d33f85e
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2023-12-20 13:05:15 +0530

    added carousel file

---
+++ src/app/(Marketing)/_components/Carousel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Carousel = () => {
+  return (
+    <div>
+      This is Carousel
+    </div>
+  )
+}
+
+export default Carousel
--- src/app/(Marketing)/page.js
+++ src/app/(Marketing)/page.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./_components/navbar";
 import JoinMeet from "./_components/JoinMeet";
+import Carousel from "./_components/Carousel";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
- <JoinMeet/>
+      <div className="">
+     <JoinMeet/>
+     <Carousel/>
+      </div>
     </div>
   );
 };
