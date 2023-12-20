7b8ae709b01d094f00d05bc37c0cebc15bc5ed15
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2023-12-20 12:26:11 +0530

    Updated JoinMeet

--- src/app/(Marketing)/_components/JoinMeet.js
+++ src/app/(Marketing)/_components/JoinMeet.js
@@ -3,35 +3,39 @@ import React from "react";
 
 const JoinMeet = () => {
   return (
-    <div className="mx-20 " style={{marginTop:'150px'}}>
+    <div className=" mt-28 ml-12 ">
       {/* heading */}
       <div>
-      <h1 style={{fontSize:'40px',fontWeight:'50px'}}>Secure video conferencing for everyone</h1>
+        <div className="text-5xl ">Secure video conferencing for everyone</div>
       </div>
+
       {/* sub-heading */}
       <div>
-      <h3 style={{fontSize:'17px',color:'grey',marginTop:'15px',marginBottom:'100px'}}>
-        Connect, collaborate, and celebrate from anywhere with Google Meet
-      </h3>
+        <div className=" text-slate-500 mt-5 mb-[40px] ">
+          Connect, collaborate, and celebrate from anywhere with Google Meet
+        </div>
       </div>
+
       <div className="d-flex">
-
-      {/* new meeting button */}
-       <Button >New meeting</Button>
-      {/* MEETING CODE INPUT */}
-    <input className="mx-3" type="text" placeholder="Enter a code or nickname"/>
-      {/* Join button */}
-       <button>Join</button>
-      </div> 
-
+        {/* new meeting button */}
+        <button className=" bg-blue-500 p-3 rounded  ">New meeting</button>
+        {/* MEETING CODE INPUT */}
+        <input
+          className="mx-3 p-4 border-slate-500 rounded border-2 w-40 h-[50px] hover:border-sky-500 "
+          type="text"
+          placeholder="Enter a code or nickname"
+        />
+        {/* Join button */}
+        <button className=" text-slate-300 ">Join</button>
+      </div>
+      {/* Break Line */}
+      <div className="my-4 border-slate-300 max-w-sm  border-b-2  "></div>
       {/* Learn more */}
       <span>
-      <a
-        href="https://meet.google.com/about/redirect/landing-learn-more/?hl=en"
-      >
-        Learn more
-      </a>
-       <span className="mx-1">about Google Meet</span> 
+        <a className="text-blue-500" href="https://meet.google.com/about/redirect/landing-learn-more/?hl=en">
+          Learn more
+        </a>
+        <span className="mx-1">about Google Meet</span>
       </span>
     </div>
   );
